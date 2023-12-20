a35293c3656d8720e29361c8302612f3b2372eee
mudassirshah1335 <mudassirshar1335@gmail.com>
2023-12-19 20:30:20 -0800

    today task in typescript

---
+++ 18-12-Types/app.ts
@@ -0,0 +1,51 @@
+// let num1 = 1;
+
+// function calculate(num1: number, num2: number) {
+//     return num1 + num2;
+// }
+
+// console.log(calculate(num1, 3));
+
+// function getTotal(numbers: Array<number>) {
+//     return numbers.reduce((acc, item) => {
+//         return acc + item;
+//     }, 0);
+// }
+
+// console.log(getTotal([3, 2, 1]));
+
+// const user = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     role: 'professor',
+// };
+// console.log(user);
+
+// number
+// string
+// arr[]
+// boolean
+
+// Type alias
+
+// type User = {
+//     name: string;
+//     age: number;
+//     address?: string;
+// };
+
+// function login(userData: User): User {
+//     return userData;
+// }
+
+// const user = {
+//     name: 'mudassir K',
+//     age: 30,
+// };
+
+// console.log(login(user));
+
+// type ID = number | string;
+
+// const userId: ID = '123';
+
